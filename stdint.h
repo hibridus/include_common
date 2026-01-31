092f3509851fdfffd3f74dd25267153243d05a53
cabritoing <carbritomestre@outlook.com>
2026-01-31 10:46:10 -0300

    feat: First commit

---
+++ stdint.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDE_COMMON_STDINT_H
+#define INCLUDE_COMMON_STDINT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/* 8 bits */
+typedef signed char   INT8;
+typedef unsigned char UINT8, CHAR, BYTE, BOOL;
+
+/* 16 bits */
+typedef signed short   INT16;
+typedef unsigned short UINT16, SHORT, WORD;
+
+/* 32 bits */
+typedef signed int   INT32;
+typedef unsigned int UINT32, DWORD;
+
+/* 64 bits */
+typedef signed long long   INT64;
+typedef unsigned long long UINT64, QWORD;
+
+/* Pointers */
+typedef void* PVOID, LPVOID;
+typedef CHAR* STRING;
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // INCLUDE_COMMON_STDINT_H
